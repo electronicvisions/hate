f6616411b0e0e971d91c8561b53495053b94b860
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-02-19 18:44:25 +0000

    Add compile-time integer round-up to multiples and division
    Change-Id: I0f789bb1faef1642a869ef6e4a8daf687fed7672

--- include/hate/math.h
+++ include/hate/math.h
@@ -14,5 +14,27 @@ constexpr size_t num_bits(size_t n)
 	return ((n < 2) ? 1 : (1 + num_bits(n / 2)));
 }
 
+/**
+ * @brief Calculates the integer division rounded up.
+ * @param number Number to calculate division for
+ * @param divisor Divisor
+ * @return Result of division
+ */
+constexpr size_t round_up_integer_division(size_t number, size_t divisor)
+{
+	return (number + divisor - 1) / divisor;
+}
+
+/**
+ * @brief Rounds up a number to full multiples.
+ * @param number Number to round up
+ * @param multiple Multiple to use as base for rounding
+ * @return Rounded number
+ */
+constexpr size_t round_up_to_multiple(size_t number, size_t multiple)
+{
+	return round_up_integer_division(number, multiple) * multiple;
+}
+
 } // namespace math
 } // namespace hate
--- tests/test-math.cpp
+++ tests/test-math.cpp
@@ -9,3 +9,25 @@ TEST(NumBits, General) {
 	EXPECT_EQ(num_bits(7), 3);
 	EXPECT_EQ(num_bits(8), 4);
 }
+
+TEST(round_up_integer_division, General)
+{
+	using namespace hate::math;
+	EXPECT_EQ(round_up_integer_division(7, 7), 1);
+	EXPECT_EQ(round_up_integer_division(7, 8), 1);
+	EXPECT_EQ(round_up_integer_division(8, 7), 2);
+
+	// compile-test
+	__attribute__((unused)) constexpr size_t res = round_up_integer_division(10, 7);
+}
+
+TEST(round_up_to_multiple, General)
+{
+	using namespace hate::math;
+	EXPECT_EQ(round_up_to_multiple(7, 7), 7);
+	EXPECT_EQ(round_up_to_multiple(7, 8), 8);
+	EXPECT_EQ(round_up_to_multiple(8, 7), 14);
+
+	// compile-test
+	__attribute__((unused)) constexpr size_t res = round_up_to_multiple(10, 7);
+}
