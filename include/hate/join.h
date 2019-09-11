30e2e2d2f28e570aa0c268537d8b2e83d416c31f
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-09-11 16:40:09 +0200

    Add join{._string} to join iterable with delimiter
    Change-Id: If6b81e6d0bf50a19bd6e7065d5b3c81829913cb1

---
+++ include/hate/join.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <sstream>
+#include <string>
+
+namespace hate {
+
+/**
+ * Join iterable entries with delimiter in-between.
+ * @tparam OstreamT Sink providing ostream-operator interface
+ * @tparam IterableT Type of iterable
+ * @tparam DelimiterT Type of delimiter
+ * @param os OstremT instance to be modified
+ * @param iterable Iterable to join
+ * @param delimiter Delimiter to intert between iterable elements
+ */
+template <typename OstreamT, typename IterableT, typename DelimiterT>
+void join(OstreamT& os, IterableT const& iterable, DelimiterT const& delimiter)
+{
+	for (auto it = iterable.cbegin(); it != iterable.cend(); ++it) {
+		os << *it;
+		if (it != iterable.cend() - 1) {
+			os << delimiter;
+		}
+	}
+}
+
+/**
+ * Join iterable to string with delimiter in-between.
+ * @tparam IterableT Type of iterable
+ * @tparam DelimiterT Type of delimiter
+ * @param iterable Iterable instance to join
+ * @param delimiter Delimiter to use in-between
+ */
+template <typename IterableT, typename DelimiterT>
+std::string join_string(IterableT const& iterable, DelimiterT const& delimiter)
+{
+	std::stringstream ss;
+	join(ss, iterable, delimiter);
+	return ss.str();
+}
+
+} // namespace hate
---
+++ tests/test-join.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "hate/join.h"
+
+#include <array>
+#include <sstream>
+#include <string>
+
+TEST(JoinString, Array)
+{
+	std::array<int, 3> iterable = {1, 2, 3};
+	std::string delimiter = ", ";
+
+	std::stringstream ss;
+	ss << "[" << hate::join_string(iterable, delimiter) << "]";
+	EXPECT_EQ(ss.str(), "[1, 2, 3]");
+}
+
+TEST(Join, Array)
+{
+	std::array<int, 3> iterable = {1, 2, 3};
+	std::string delimiter = ", ";
+
+	std::stringstream ss;
+	ss << "[";
+	hate::join(ss, iterable, delimiter);
+	ss << "]";
+	EXPECT_EQ(ss.str(), "[1, 2, 3]");
+}
