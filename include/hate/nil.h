946cc49307f8b9604b4b7573a52fa3ed474185da
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2019-10-22 19:29:58 +0200

    Add Nil empty structure
    Change-Id: I64405216bed149930726b550ef000c9009c2f221

---
+++ include/hate/nil.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace hate {
+
+/**
+ * Empty structure to be used in APIs where a type is expected, but no reasonable implementation
+ * can be provided.
+ */
+struct Nil
+{};
+
+} // namespace hate
