2f15b0d7dca9829c3c3850929b5c6405035537cb
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-03-06 15:53:18 +0100

    Add overloaded helper from <variant>
    * new header "hate/variant.h"
    
    * provides `overloaded` helper that can be used in `std::visit`:
    ```c++
      std::visit(overloaded {
          [](auto arg) { std::cout << arg << ' '; },
          [](double arg) { std::cout << std::fixed << arg << ' '; },
          [](const std::string& arg) { std::cout << std::quoted(arg) << ' '; },
      }, v);
    ```
    
    * no tests since it's just copy-paste
    
    Change-Id: I15b9b0df1d7943995258c7bbfdd420787b39be66

---
+++ include/hate/variant.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace hate {
+
+// copied from https://en.cppreference.com/w/cpp/utility/variant/visit
+template <class... Ts>
+struct overloaded : Ts...
+{
+	using Ts::operator()...;
+};
+template <class... Ts>
+overloaded(Ts...)->overloaded<Ts...>; // not needed as of C++20
+
+}
