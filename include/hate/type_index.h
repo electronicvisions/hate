5d7728bdfd1e553153d1b18e026279aaa59b5c13
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-05-18 21:15:13 +0200

    Add {full_}name function
    Change-Id: I9348f81d382c219e3d85a1889357a3c5761e149e

---
+++ include/hate/type_index.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <boost/type_index.hpp>
+
+namespace hate {
+
+/**
+ * Get fully qualified name of a type including namespaces.
+ * The format follows "name::space::Type::SubType<TParams...>".
+ * @tparam T Type to get name for
+ */
+template <typename T>
+std::string full_name()
+{
+	return boost::typeindex::type_id<T>().pretty_name();
+}
+
+/**
+ * Get name of a type stripped of namespaces and surrounding classes.
+ * The format follows "SubType<TParams...>" for name::space::Type::SubType<Tparams...>.
+ * @tparam T Type to get name for
+ */
+template <typename T>
+std::string name()
+{
+	auto const full = full_name<T>();
+	return full.substr(full.rfind(':') + 1);
+}
+
+} // namespace hate
---
+++ tests/test-type_index.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "hate/type_index.h"
+
+struct SomeThing
+{};
+
+namespace some::where {
+struct OtherThing
+{};
+} // some::where
+
+TEST(full_name, General)
+{
+	EXPECT_EQ(hate::full_name<SomeThing>(), "SomeThing");
+	EXPECT_EQ(hate::full_name<some::where::OtherThing>(), "some::where::OtherThing");
+}
+
+TEST(name, General)
+{
+	EXPECT_EQ(hate::name<SomeThing>(), "SomeThing");
+	EXPECT_EQ(hate::name<some::where::OtherThing>(), "OtherThing");
+}
