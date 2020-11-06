f8718505f8f39d65537122bb28c59b2581e0106e
Eric Müller <mueller@kip.uni-heidelberg.de>
2020-11-06 14:49:45 +0100

    Provide begin/end-iterarator based join{,_string} versions
    Change-Id: I90689f790f50664ccec9962cfb2eb62fd062c522

--- include/hate/join.h
+++ include/hate/join.h
@@ -5,6 +5,27 @@
 namespace hate {
 
 /**
+ * Join an iterator range of entries with delimiter in-between.
+ * @tparam OstreamT Sink providing ostream-operator interface
+ * @tparam IteratorT Type of begin/end iterators
+ * @tparam DelimiterT Type of delimiter
+ * @param os OstreamT instance to be modified
+ * @param begin Iterator pointing to begin
+ * @param end Iterator pointing to end
+ * @param delimiter Delimiter to insert between iterable elements
+ */
+template <typename OstreamT, typename IteratorT, typename DelimiterT>
+void join(OstreamT& os, IteratorT const& begin, IteratorT const& end, DelimiterT const& delimiter)
+{
+	for (auto it = begin; it != end; ++it) {
+		os << *it;
+		if (auto tit = it; ++tit != end) {
+			os << delimiter;
+		}
+	}
+}
+
+/**
  * Join iterable entries with delimiter in-between.
  * @tparam OstreamT Sink providing ostream-operator interface
  * @tparam IterableT Type of iterable
@@ -16,12 +37,7 @@ namespace hate {
 template <typename OstreamT, typename IterableT, typename DelimiterT>
 void join(OstreamT& os, IterableT const& iterable, DelimiterT const& delimiter)
 {
-	for (auto it = iterable.begin(); it != iterable.end(); ++it) {
-		os << *it;
-		if (it != iterable.end() - 1) {
-			os << delimiter;
-		}
-	}
+	join(os, iterable.begin(), iterable.end(), delimiter);
 }
 
 /**
@@ -36,6 +52,22 @@ std::string join_string(IterableT const& iterable, DelimiterT const& delimiter)
 {
 	std::stringstream ss;
 	join(ss, iterable, delimiter);
+	return ss.str();
+}
+
+/**
+ * Join iterable to string with delimiter in-between.
+ * @tparam IteratorT Iterator type
+ * @tparam DelimiterT Type of delimiter
+ * @param begin Iterator pointing to begin
+ * @param end Iterator pointing to end
+ * @param delimiter Delimiter to use in-between
+ */
+template <typename IteratorT, typename DelimiterT>
+std::string join_string(IteratorT const& begin, IteratorT const& end, DelimiterT const& delimiter)
+{
+	std::stringstream ss;
+	join(ss, begin, end, delimiter);
 	return ss.str();
 }
 
--- tests/test-join.cpp
+++ tests/test-join.cpp
@@ -10,9 +10,17 @@ TEST(JoinString, Array)
 	std::array<int, 3> iterable = {1, 2, 3};
 	std::string delimiter = ", ";
 
-	std::stringstream ss;
-	ss << "[" << hate::join_string(iterable, delimiter) << "]";
-	EXPECT_EQ(ss.str(), "[1, 2, 3]");
+	{
+		std::stringstream ss;
+		ss << "[" << hate::join_string(iterable, delimiter) << "]";
+		EXPECT_EQ(ss.str(), "[1, 2, 3]");
+	}
+
+	{
+		std::stringstream ss;
+		ss << "[" << hate::join_string(iterable.begin(), iterable.end(), delimiter) << "]";
+		EXPECT_EQ(ss.str(), "[1, 2, 3]");
+	}
 }
 
 TEST(Join, Array)
@@ -20,9 +28,19 @@ TEST(Join, Array)
 	std::array<int, 3> iterable = {1, 2, 3};
 	std::string delimiter = ", ";
 
-	std::stringstream ss;
-	ss << "[";
-	hate::join(ss, iterable, delimiter);
-	ss << "]";
-	EXPECT_EQ(ss.str(), "[1, 2, 3]");
+	{
+		std::stringstream ss;
+		ss << "[";
+		hate::join(ss, iterable, delimiter);
+		ss << "]";
+		EXPECT_EQ(ss.str(), "[1, 2, 3]");
+	}
+
+	{
+		std::stringstream ss;
+		ss << "[";
+		hate::join(ss, iterable.begin(), iterable.end(), delimiter);
+		ss << "]";
+		EXPECT_EQ(ss.str(), "[1, 2, 3]");
+	}
 }
